ef9200a28b6ae0f349c6cddc3cf0158fbb39daa8
yoochan0108 <yoochan0108@naver.com>
2023-10-05 16:14:32 +0900

    지도하나에 필요한 모든 정보를 객체로 그룹화

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,36 +1,32 @@
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 import { useRef, useEffect } from 'react';
-
 export default function Contact() {
 	const map = useRef(null);
 	//현재 kakao객체를 cdn으로 가져오고 있기 때문에
-	//리엑트 컴포넌트안쪽에서 window객체를 비구조화할당을 이용해서 수동으로 꺼내옴.
+	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
-	const mapOption = {
-		center: new kakao.maps.LatLng(37.5847929021772, 126.88566422376147),
-		level: 1,
+	const info = {
+		latlng: new kakao.maps.LatLng(37.58478163978524, 126.88566424098676),
+		imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 99) },
 	};
-	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
-	const imageSize = new kakao.maps.Size(232, 99);
-
-	const imageOption = { offset: new kakao.maps.Point(116, 90) };
-
-	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
 	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
 	const marker = new kakao.maps.Marker({
-		position: mapOption.center,
-		image: markerImage,
+		position: info.latlng,
+		image: new kakao.maps.MarkerImage(info.imgSrc, info.imgSize, info.imgPos),
 	});
 
 	useEffect(() => {
 		//컴포넌트 마운트 되자마자 지도인스턴스 생성
-		const instance = new kakao.maps.Map(map.current, mapOption);
+		const instance = new kakao.maps.Map(map.current, {
+			center: info.latlng,
+			level: 1,
+		});
 		//마커 출력 인스턴스에 지도 인스턴스 결합
 		marker.setMap(instance);
 	}, []);
-
 	return (
 		<Layout title={'Contact'}>
 			<div className='map' ref={map}></div>
