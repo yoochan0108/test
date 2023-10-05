cb519641ef4584f70219b0d3bee60c81c87aca7e
yoochan0108 <yoochan0108@naver.com>
2023-10-05 16:33:50 +0900

    교통 정보 기능 토글로 통합

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,8 +1,11 @@
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 export default function Contact() {
 	const map = useRef(null);
+	const instance = useRef(null);
+	const [Traffic, setTraffic] = useState(false);
+
 	//현재 kakao객체를 cdn으로 가져오고 있기 때문에
 	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
@@ -12,6 +15,7 @@ export default function Contact() {
 		imgSize: new kakao.maps.Size(232, 99),
 		imgPos: { offset: new kakao.maps.Point(116, 99) },
 	};
+
 	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
 	const marker = new kakao.maps.Marker({
 		position: info.latlng,
@@ -20,15 +24,28 @@ export default function Contact() {
 
 	useEffect(() => {
 		//컴포넌트 마운트 되자마자 지도인스턴스 생성
-		const instance = new kakao.maps.Map(map.current, {
+		instance.current = new kakao.maps.Map(map.current, {
 			center: info.latlng,
 			level: 1,
 		});
 		//마커 출력 인스턴스에 지도 인스턴스 결합
-		marker.setMap(instance);
+		marker.setMap(instance.current);
 	}, []);
+
+	useEffect(() => {
+		//traffic 값이 바뀔때마다 실행될 구문
+		Traffic
+			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
+
 	return (
 		<Layout title={'Contact'}>
+			{/* <button onClick={() => setTraffic(true)}>주변 교통정보 보기</button>
+			<button onClick={() => setTraffic(false)}>주변 교통정보 끄기</button> */}
+			<button onClick={() => setTraffic(!Traffic)}>
+				{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
+			</button>
 			<div className='map' ref={map}></div>
 		</Layout>
 	);
