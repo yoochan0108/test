9db3889788d95a5154cd8e8628993483c189b2dd
yoochan0108 <yoochan0108@naver.com>
2023-10-10 10:32:20 +0900

    모든 오류 수정 완료본(+data fetching함수 async await로 개선)

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,19 +5,26 @@ import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	const api_key = 'b6c85d56ee255dee970b2241f275f09d';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 50;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+	const fetchData = async () => {
+		const api_key = '2a1a0aebb34012a99c23e13b49175343';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 50;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+		//만약 특정함수가 promise를 반환한다면 warpping함수로 묶어준뒤 async 지정
+		//각각의 promise 반환 함수 앞쪽에 await를 붙이기만 하면 해당 코드는 동기화됨
+		//지저분하게 depth를 들여쓰기 해가면서 then구문을 호출할 필요가 없음
+		const data = await fetch(url);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
 
 	useEffect(() => {
-		fetch(url)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.photos.photo);
-				setPics(json.photos.photo);
-			});
+		fetchData();
 	}, []);
+
 	return (
 		<Layout title={'Gallery'}>
 			<div className='picFrame'>
