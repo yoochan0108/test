6d50ce090f6d4aed77554c54b4e912559dd34858
yoochan0108 <yoochan0108@naver.com>
2023-10-10 13:51:31 +0900

    모든 이미지 소스 렌더링 완료 유무를 onload이벤트로 탐색

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
+	const refFrame = useRef(null);
 	const refInput = useRef(null);
 	const [Pics, setPics] = useState([]);
+	const [Loader, setLoader] = useState(true);
 	const my_id = '199272370@N07';
 
 	const fetchData = async (opt) => {
@@ -33,38 +35,54 @@ export default function Gallery() {
 			return alert('해당 검색어의 결과값이 없습니다.');
 		}
 		setPics(json.photos.photo);
+		let count = 0;
+		const imgs = refFrame.current?.querySelectorAll('img');
+		//실제 데이터가 state에 담기는 순간 가상돔이 생성되는 순간
+		imgs.forEach((img, idx) => {
+			img.onload = () => {
+				++count;
+				console.log('현재 로딩된 img갯수', count);
+				if (count === imgs.length) {
+					console.log('모든 이미지 소스 렌더링 완료!');
+					//모든 소스이미지라 랜더링 완료되면 Loader값을
+					//false로 바꿔서 로딩이미지 제거
+					setLoader(false);
+				}
+			};
+		});
 	};
 
 	useEffect(() => {
-		//type: 'interest' 인터레스트 방식 갤러리 호출
-		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		//type: 'search'검색키워드로 갤러리 호출
 		fetchData({ type: 'user', id: my_id });
-		//fetchData({ type: 'interest' });
-		//fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
-			<form
-				onSubmit={(e) => {
-					//submit이벤트의 기본 서버 전송기능을 막아줌
-					e.preventDefault();
-					//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
-					if (refInput.current.value.trim() === '') {
-						return alert('검색어를 입력하세요.');
-					}
+			<div className='searchBox'>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						if (refInput.current.value.trim() === '') {
+							return alert('검색어를 입력하세요.');
+						}
+						fetchData({ type: 'search', tags: refInput.current.value });
+						refInput.current.value = '';
+					}}
+				>
+					<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+					<button>검색</button>
+				</form>
+			</div>
+			<div className='btnSet'>
+				<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
+				<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
+			</div>
 
-					fetchData({ type: 'search', tags: refInput.current.value });
-					refInput.current.value = '';
-				}}
-			>
-				<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
-				<button>검색</button>
-			</form>
-			<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
-			<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
-			<div className='picFrame'>
+			{/* Loader가 true일때에만 로딩 이미지 출력 */}
+			{Loader && (
+				<img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loading' />
+			)}
+			<div className='picFrame' ref={refFrame}>
 				<Masonry
 					elementType={'div'}
 					options={{ transitionDuration: '0.5s' }}
@@ -81,6 +99,7 @@ export default function Gallery() {
 										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
 									/>
 									<h2>{data.title}</h2>
+
 									<div className='profile'>
 										<img
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
