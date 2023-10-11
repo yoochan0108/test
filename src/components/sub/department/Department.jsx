c16542f1a80df5cec80df1ec1c0a61847c155688
yoochan0108 <yoochan0108@naver.com>
2023-10-11 13:48:07 +0900

    리액트 컴포넌트가 렌더링되는 흐름

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -2,9 +2,6 @@ import Layout from '../../common/layout/Layout';
 import { useEffect, useState } from 'react';
 import './Department.scss';
 const path = process.env.PUBLIC_URL;
-//1. useEffect로 컴포넌트 마운트가되자마자 fatch외부데이터 가져옴
-//2. 데이터가 다 받아지면 useState로 state에 해당 담아줌
-//3. return문 안쪽에 state값을 map으로 반복들면서 JSX출력
 
 export default function Department() {
 	const [Department, setDepartment] = useState([]);
@@ -36,3 +33,23 @@ export default function Department() {
 		</Layout>
 	);
 }
+
+/*
+	1.hook의 개념
+	- 리엑트에서는 크게 2가지 종류의 파일이 존재
+	-- 컴포넌트 (화면에 가상돔을 랜더링하는 JSX를 무조건 리턴)
+	-- hook (JSX를 리턴하는것이 아닌 각 컴포넌트마다 자주쓰는 기능의 함수나 특정 값을 리턴하는 기능 파일)
+
+	2.useState, useEffect, useRef (리엑트에서 제일 많이 쓰는 기본 hook)
+	- useState: 화면에 랜더링을 담당하는 중요한 정보를 담아주고 변경해주는 기능의 훅 (State가 변경되면 컴포넌트는 제호출되면서 화면 재랜더링)
+	- useEffect: 컴포넌트의 생성, 변경, 소멸시마다 (컴포넌트의 생명주기마다) 특정 구문을 호출할 수 있는 hook
+	-useRef: 참조객체에 실시간으로 특정 정보값을 담기위한 hook (해당 랜더링 사이클에서 최신 가상돔을 담을때, 특정값을 담아두고 변경을 할떄 컴포넌트를 재랜더링 시키고 싶지 않을 때 (모션))
+
+	3.컴포넌트가 하는역할 (JSX)
+	- 자바스크립트로 동적 돔을 만들 때 편의성을 위해 HTML형식을 따와서 편하게 동적 DOM 생성을 위한 리엑트만의 표현식
+
+	4.fetch문을 useEffect안쪽에서 호출하는 이유
+	- 가상돔 생성은 리엑트 기반의 스크립트가 처리해주시면 외부 데이터를 가져오는 것은 Web API (브라우저)가 처리가기 때문에
+	- 컴포넌트가 실제 브라우저상에 마운트가되고 브라우저가 작업 준비가 되야지만 fetch를 실행할 수 있기 때문에
+	- useEffect컴포넌트가 마운트 되야지만 CSR방식으로 외부 데이터를 가져올 수 있음.
+*/
