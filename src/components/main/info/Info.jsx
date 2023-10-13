de86652314958a7a68850e6eea426576d2e33f11
yoochan0108 <yoochan0108@naver.com>
2023-10-13 15:49:41 +0900

    그리드박스 스타일링

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -3,7 +3,20 @@ import './Info.scss';
 function Info() {
 	return (
 		<section className='info'>
-			<h2>Information</h2>
+			<div className='wrap'>
+				<article>1</article>
+				<article>2</article>
+				<article>3</article>
+				<article>4</article>
+				<article>5</article>
+				<article>6</article>
+				<article>7</article>
+				<article>8</article>
+				<article>9</article>
+				<article>10</article>
+				<article>11</article>
+				<article>12</article>
+			</div>
 		</section>
 	);
 }
