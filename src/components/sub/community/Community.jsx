fea7554dd894b63137619a8ce7d359c3d6ecd5a7
yoochan0108 <yoochan0108@naver.com>
2023-10-13 10:32:52 +0900

    showBox 스타일링

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -20,13 +20,14 @@ export default function Community() {
 		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
 		resetForm();
 	};
+
 	const deletePost = (delIndex) => {
-		//기존에 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
-		setPosts(Posts.filter((_, idx) => delIndex === idx));
+		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
+		setPosts(Posts.filter((_, idx) => delIndex !== idx));
 	};
 
 	return (
-		<Layout title={'Communtiy'}>
+		<Layout title={'Community'}>
 			<div className='inputBox'>
 				<input ref={refInput} type='text' placeholder='제목을 입력하세요.' />
 				<br />
@@ -42,8 +43,10 @@ export default function Community() {
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
-							<h2>{post.title}</h2>
-							<p>{post.content}</p>
+							<div className='txt'>
+								<h2>{post.title}</h2>
+								<p>{post.content}</p>
+							</div>
 
 							<nav className='btnSet'>
 								<button>Edit</button>
