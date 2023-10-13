cb79c48b56e94c4440ec2c9abdee8a0bca9f99b2
yoochan0108 <yoochan0108@naver.com>
2023-10-13 15:39:42 +0900

    메인페이지에 미리보기 출력

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -1,9 +1,27 @@
 import './News.scss';
+import { useState } from 'react';
 
 function News() {
+	const getLocalDate = () => {
+		const data = localStorage.getItem('post');
+		return JSON.parse(data);
+	};
+	const [Post, setPost] = useState(getLocalDate());
 	return (
 		<section className='news'>
 			<h2>News</h2>
+			<div className='postWrap'>
+				{Post.map((el, idx) => {
+					if (idx >= 4) return null;
+					else
+						return (
+							<article key={idx}>
+								<h2>{el.title}</h2>
+								<p>{el.content}</p>
+							</article>
+						);
+				})}
+			</div>
 		</section>
 	);
 }
