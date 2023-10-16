53df7959832aac76978444fa84995fb7e8527ae0
yoochan0108 <yoochan0108@naver.com>
2023-10-16 09:38:14 +0900

    기본 인풋요소 입력

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,7 +4,40 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<p>회원가입 페이지입니다.</p>
+			<form action=''>
+				<legend>회원가입 폼 양식</legend>
+				<table border='1'>
+					<body>
+						{/* userid */}
+						<tr>
+							<th scope='row'>
+								<label htmlFor='userId'>userid</label>
+							</th>
+							<td>
+								<input type='text' id='userId' name='userid' />
+							</td>
+						</tr>
+						{/* userid */}
+						<tr>
+							<th scope='row'>
+								<label htmlFor='pwd1'>password</label>
+							</th>
+							<td>
+								<input type='password' id='pwd1' name='pwd1' />
+							</td>
+						</tr>
+						{/* userid */}
+						<tr>
+							<th scope='row'>
+								<label htmlFor='pwd2'>re-password</label>
+							</th>
+							<td>
+								<input type='re-password' id='pwd2' name='pwd2' />
+							</td>
+						</tr>
+					</body>
+				</table>
+			</form>
 		</Layout>
 	);
 }
