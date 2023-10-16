4a85ff754babc7f8f98e269ebc6cb3f27bd4048b
yoochan0108 <yoochan0108@naver.com>
2023-10-16 10:48:45 +0900

    기본 인증 로직 완성

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -9,7 +9,6 @@ export default function Members() {
 		pwd2: '',
 		email: '',
 	};
-
 	const [Val, setVal] = useState(initVal);
 
 	const handleChange = (e) => {
@@ -18,85 +17,104 @@ export default function Members() {
 	};
 
 	//인수값으로 state를 전달받아서 각 데이터별로 인증처리후
-	//만약 인증에러가 방생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
-	const check = (value) => {};
+	//만약 인증에러가 발생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
+	const check = (value) => {
+		const errs = {};
+		if (value.userid.length < 5) {
+			errs.userid = '아이디는 최소 5글자 이상 입력하세요.';
+		}
+		return errs;
+	};
 
-	//전송이벤트 발생시 state에 있는 모든 input 값들을 check함수에 전달해서 호출
+	//전송이벤트 발생시 state에 있는 인풋값들을 check함수에 전달해서 호출
 	//만약 check함수가 에러객체를 하나도 내보내지 않으면 인증성공
-	//하나라도 에러객체가 전달되면 인증 실패처리하면서 name 값과 매칭이 되는 input요소 아래쪽에 에러메세지 출력
+	//하나라도 에러객체가 전달되면 인증실패처리하면서 name값과 매칭이 되는 input요소 아래쪽에 에러메세지 출력
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		console.log(check(Val));
 	};
 
 	return (
 		<Layout title={'Members'}>
 			<form onSubmit={handleSubmit}>
-				<legend className='h'>회원가입 폼 양식</legend>
-				<table border='1'>
-					<body>
-						{/* userid */}
-						<tr>
-							<th scope='row'>
-								<label htmlFor='userId'>userid</label>
-							</th>
-							<td>
-								<input type='text' id='userId' name='userid' value={Val.userid} />
-							</td>
-						</tr>
-						{/* password */}
-						<tr>
-							<th scope='row'>
-								<label htmlFor='pwd1'>password</label>
-							</th>
-							<td>
-								<input
-									type='password'
-									id='pwd1'
-									name='pwd1'
-									value={Val.pwd1}
-									onChange={handleChange}
-								/>
-							</td>
-						</tr>
-						{/* re-password */}
-						<tr>
-							<th scope='row'>
-								<label htmlFor='pwd2'>re-password</label>
-							</th>
-							<td>
-								<input
-									type='password'
-									id='pwd2'
-									name='pwd2 '
-									value={Val.pwd2}
-									onChange={handleChange}
-								/>
-							</td>
-						</tr>
-						{/* email */}
-						<tr>
-							<th scope='row'>
-								<label htmlFor='email'>e-mail</label>
-							</th>
-							<td>
-								<input
-									type='text'
-									id='email'
-									name='email'
-									value={Val.email}
-									onChange={handleChange}
-								/>
-							</td>
-						</tr>
-						{/* btnSet */}
-						<tr>
-							<th colSpan='2'>
-								<input type='reset' Value='cancel' />
-								<input type='submit' value='send' />
-							</th>
-						</tr>
-					</body>
-				</table>
+				<fieldset>
+					<legend className='h'>회원가입 폼 양식</legend>
+					<table border='1'>
+						<tbody>
+							{/* userid */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>userid</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>password</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										id='pwd1'
+										name='pwd1'
+										value={Val.pwd1}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* re password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>re-password</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										id='pwd2'
+										name='pwd2'
+										value={Val.pwd2}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>e-mail</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										value={Val.email}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* btnSet */}
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='cancel' />
+									<input type='submit' value='send' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
 			</form>
 		</Layout>
 	);
