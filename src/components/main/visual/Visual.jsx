e073bb8bf8d237cbc10ee3a812641d8f24d4781c
yoochan0108 <yoochan0108@naver.com>
2023-10-17 16:37:52 +0900

    메인페이지에 유튜브 데이터가 5개만 출력

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,9 +1,19 @@
 import './Visual.scss';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 function Visual() {
+	const { data } = useSelector((store) => store.youtube);
 	return (
 		<section className='visual'>
 			<h2>Visual</h2>
+			{data.map((vid, idx) => {
+				if (idx >= 5) return null;
+				return (
+					<article key={idx}>
+						<h2>{vid.snippet.title}</h2>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
