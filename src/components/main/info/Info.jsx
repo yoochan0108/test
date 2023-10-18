56515b837d7a85e9dee7f31fb097067f7c35c335
yoochan0108 <yoochan0108@naver.com>
2023-10-18 10:17:47 +0900

    메인 페이지 갤러리 출력

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -5,21 +5,26 @@ function Info() {
 	const { data } = useSelector((store) => store.flickr);
 	return (
 		<section className='info'>
-			{data.map((pic, idx) => {
-				return (
-					<article key={idx}>
-						<h2>{pic.title}</h2>
-					</article>
-				);
-			})}
 			<div className='wrap'>
+				{data.map((pic, idx) => {
+					return (
+						<article key={idx}>
+							<img
+								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								alt={pic.title}
+							/>
+						</article>
+					);
+				})}
+			</div>
+			{/* <div className='wrap'>
 				<article>1</article>
 				<article>2</article>
 				<article>3</article>
 				<article>4</article>
 				<article>5</article>
 				<article>6</article>
-			</div>
+			</div> */}
 		</section>
 	);
 }
