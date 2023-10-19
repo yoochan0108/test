53463ea1e429f2b9485eeb5cd6a3917750d7bc84
yoochan0108 <yoochan0108@naver.com>
2023-10-19 13:21:53 +0900

    swiper의 활성화 순번을 구해서 요소에 활성화 모션 처리

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,10 +1,12 @@
 import './Visual.scss';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
 import 'swiper/css';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
+	const [Index, setIndex] = useState(0);
 
 	return (
 		<section className='visual'>
@@ -12,11 +14,21 @@ function Visual() {
 				<ul>
 					{data.map((tit, idx) => {
 						if (idx >= 5) return null;
-						return <li key={idx}>{tit.snippet.title}</li>;
+						return (
+							<li key={idx} className={idx === Index ? 'on' : ''}>
+								{tit.snippet.title}
+							</li>
+						);
 					})}
 				</ul>
 			</div>
-			<Swiper slidesPerView={3} spaceBetween={50} loop={true}>
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={50}
+				loop={true}
+				//swiper loop 기능을 적용하는 순간 실제 연결되어 있는 패널갯수보다 동적으로 패널이 생성되면서 일반적인 방법으로는 활성화패널의 순서값을 구할 수 없기 떄문에 아래와 같은 방법으로 순서값을 구함
+				onSlideChange={(el) => setIndex(el.realIndex)}
+			>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
